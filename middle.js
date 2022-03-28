8ccfd52993a4e355dd84b71e238ef979bf7e6c36
Aidan Antony <aidanantony65@gmail.com>
2022-03-28 10:29:12 -0600

    My middle function with test cases

--- middle.js
+++ middle.js
@@ -1,3 +1,22 @@
+const eqArrays = function(arrayOne,arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    
+    if (arrayOne[i] !== arrayTwo[i] || arrayOne.length !== arrayTwo.length) {
+      return false;
+    }
+  
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`💯💯💯 Assertation Passed: [${actual}] === [${expected}]`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertation Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
 const middle = function(array) {
   let mid = [];
   for (let i = 0; i < array.length; i++) {
