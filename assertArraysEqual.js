0489c310ab0119b921ee5bf7e8d4f1d1a28382e8
Aidan Antony <aidanantony65@gmail.com>
2022-03-28 01:55:41 -0600

    My assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(arrayOne,arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    
+    if (arrayOne[i] !== arrayTwo[i] || arrayOne.length !== arrayTwo.length) {
+      return false;
+    }
+  
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`💯💯💯 Assertation Passed: [${actual}] === [${expected}]`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertation Failed: [${actual}] !== [${expected}]`);
+  }
+};
+assertArraysEqual([1,2,3],[1,2,3]);
