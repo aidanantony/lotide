eb9db0d1deae541f6fed6f0e023d73208c72c263
Aidan Antony <aidanantony65@gmail.com>
2022-03-28 14:11:10 -0600

    My flatten function

---
+++ flatten.js
@@ -0,0 +1,24 @@
+const eqArrays = function(arrayOne,arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    
+    if (arrayOne[i] !== arrayTwo[i] || arrayOne.length !== arrayTwo.length) {
+      return false;
+    }
+  
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`💯💯💯 Assertation Passed: [${actual}] === [${expected}]`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertation Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const flatten = function(array) {
+  return [].concat(...array);
+};
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
