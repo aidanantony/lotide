d795dcb5705fdd546f1f53b0a3012c16d34423f5
Aidan Antony <aidanantony65@gmail.com>
2022-04-01 11:27:46 -0600

    My countOnly function

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`💯💯💯 Assertation Passed: [${actual}] === [${expected}]`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertation Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+  
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+return results;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
