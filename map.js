37db60f48f0d979223e54c27d7e6b78617f7b558
Aidan Antony <aidanantony65@gmail.com>
2022-04-04 23:30:18 -0600

    My final map function with test cases

--- map.js
+++ map.js
@@ -1,13 +1,32 @@
+const eqArrays = function(arrayOne,arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    
+    if (arrayOne[i] !== arrayTwo[i] || arrayOne.length !== arrayTwo.length) {
+      return false;
+    }
+  
+  }
+  return true;
+};
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`💯💯💯 Assertation Passed: [${actual}] === [${expected}]`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertation Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
 const words = ["ground", "control", "to", "major", "tom"];
 const map = function(array,callback) {
-  const results = []
+  const results = [];
   for (let character of array) {
-    console.log('character before:', character)
-    console.log('character after:', callback(character))
-    console.log('---');
+    results.push(callback(character));
   }
-  return results
+  return results;
 };
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
+assertArraysEqual(words,words);
+assertArraysEqual(results1,['g','c','t','m','t']);
+assertArraysEqual(words,words[0]);
