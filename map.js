e87bf4b2e3ef517a4076de42967bc9b194406f49
Aidan Antony <aidanantony65@gmail.com>
2022-04-04 23:16:21 -0600

    My map function at its early stages

---
+++ map.js
@@ -0,0 +1,6 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function() {
+  // empty for now :)
+}
+const results1 = words.map(words,word => word[0]);
+console.log(results1);
