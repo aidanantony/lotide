c5bd9d5181b36a8f0239056bf51e519349d48577
Aidan Antony <aidanantony65@gmail.com>
2022-04-15 13:28:37 -0600

    My refactored eqArrays function

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💯💯💯 Assertation Passed: [${actual}] === [${expected}]`);
-  } else if (actual !== expected) {
-    console.log(`❌❌❌ Assertation Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
-
 const eqArrays = function(arrayOne,arrayTwo) {
   for (let i = 0; i < arrayOne.length; i++) {
     
@@ -17,6 +8,5 @@ const eqArrays = function(arrayOne,arrayTwo) {
   }
   return true;
 };
-//My two test cases using my assertEqual function
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, "4"]), true);
+
+module.exports= eqArrays;
