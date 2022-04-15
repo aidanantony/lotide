930cbf4ee47efc0471b6a7e403e88705ca7e5832
Aidan Antony <aidanantony65@gmail.com>
2022-04-15 12:18:51 -0600

    My refactored asserEqual function

--- assertEqual.js
+++ assertEqual.js
@@ -5,7 +5,4 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌ Assertation Failed: [${actual}] !== [${expected}]`);
   }
 };
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
